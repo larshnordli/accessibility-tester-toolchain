9635fd4384d04b623babea6fb2795f0b8acb9502
Lars Henrik Nordli <larshenrik91@gmail.com>
2017-02-08 10:58:41 +0100

    accessibility and aria-changes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,7 @@
+
 var app = angular.module("Focuser", []);
 
-app.controller("FocusController", function($scope){
+app.controller("FocusController", function($scope, $timeout){
   $scope.test = "Hi from Angular";
 
   var messages = [
@@ -13,11 +14,33 @@ app.controller("FocusController", function($scope){
   $scope.messages = messages;
 
   $scope.sendMessage = function(){
-    var message = $scope.inputField;
     
+    var message = $scope.inputField;
+
     if(message != null){
       messages.push($scope.inputField);
+      $scope.inputField = '';
+      $scope.textArea = true;
     }
 
   }
+});
+
+angular.module('ng').directive('ngFocus', function($timeout) {
+    return {
+        link: function ( scope, element, attrs ) {
+            scope.$watch( attrs.ngFocus, function ( val ) {
+                if ( angular.isDefined( val ) && val ) {
+                    $timeout( function () { element[0].focus(); } );
+                }
+            }, true);
+
+            element.bind('blur', function () {
+                if ( angular.isDefined( attrs.ngFocusLost ) ) {
+                    scope.$apply( attrs.ngFocusLost );
+
+                }
+            });
+        }
+    };
 });
